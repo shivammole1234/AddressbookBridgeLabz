0a1b8aacde7324b24a942711f203f08647dc02fd
Shivam Mole <shivammole1713@gmail.com>
2024-09-16 12:20:18 +0530

    first commit on address book

---
+++ java/brigelabz/addressbook/address_book_bridgelabz.java
@@ -0,0 +1,7 @@
+package brigelabz.addressbook;
+
+public class address_book_bridgelabz {
+    public static void main(String[] args) {
+        System.out.println("welcome to address book code ");
+    }
+}
